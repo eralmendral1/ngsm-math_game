d94719cffa442b3e790454fb2d37befe1b32c28d
m1racl3 <eralmendral@gmail.com>
2024-01-03 08:32:39 +0800

    handling correct answer

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,26 +1,40 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { AbstractControl, FormControl, FormGroup } from '@angular/forms'
 import { MathValidators } from '../math-validators'
+import { delay, filter } from 'rxjs'
 
 @Component({
     selector: 'app-equation',
     templateUrl: './equation.component.html',
     styleUrls: ['./equation.component.css']
 })
-export class EquationComponent {
+export class EquationComponent implements OnInit {
     mathForm = new FormGroup({
-        a: new FormControl(''),
-        b: new FormControl(''),
+        a: new FormControl(this.randomNumber()),
+        b: new FormControl(this.randomNumber()),
         answer: new FormControl('')
     }, [
         MathValidators.addition('answer', 'a', 'b')
     ])
 
+    ngOnInit(): void {
+        this.mathForm.statusChanges.pipe(filter(value => value === 'VALID'), delay(200))
+            .subscribe(value => {
+                this.mathForm.setValue({
+                    a: this.randomNumber(),
+                    b: this.randomNumber(),
+                    answer: ''
+                })
+            })
+    }
+
     get a() {
+        console.log(this.mathForm.value.a)
         return this.mathForm.value.a
     }
 
     get b() {
+        console.log(this.mathForm.value.b)
         return this.mathForm.value.b
     }
 
--- src/app/math-validators.ts
+++ src/app/math-validators.ts
@@ -2,11 +2,10 @@ import { AbstractControl } from '@angular/forms'
 
 export class MathValidators {
     static addition(target: string, sourceOne: string, sourceTwo: string) {
-
         return (form: AbstractControl) => {
-            const sum = form.value.target
-            const firstNum = form.value.sourceOne
-            const secondNum = form.value.sourceTwo
+            const sum = form.value[target]
+            const firstNum = parseInt(form.value[sourceOne])
+            const secondNum = parseInt(form.value[sourceTwo])
 
             if (firstNum + secondNum === parseInt(sum)) return null
 
