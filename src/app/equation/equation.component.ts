30ba4a5a543d9c56915ee693e0146754ec97c773
m1racl3 <eralmendral@gmail.com>
2024-01-03 08:15:19 +0800

    reusable custom math validator

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms'
+import { Component } from '@angular/core'
+import { AbstractControl, FormControl, FormGroup } from '@angular/forms'
+import { MathValidators } from '../math-validators'
 
 @Component({
-  selector: 'app-equation',
-  templateUrl: './equation.component.html',
-  styleUrls: ['./equation.component.css']
+    selector: 'app-equation',
+    templateUrl: './equation.component.html',
+    styleUrls: ['./equation.component.css']
 })
 export class EquationComponent {
     mathForm = new FormGroup({
         a: new FormControl(''),
         b: new FormControl(''),
         answer: new FormControl('')
-    })
-    
+    }, [
+        MathValidators.addition('answer', 'a', 'b')
+    ])
+
     get a() {
         return this.mathForm.value.a
     }
